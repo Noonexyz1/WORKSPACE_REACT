8fdebee14969f047a0498da591b8293d66a752b6
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-05 11:44:47 -0400

    Proyecto CustomClotes iniciado

--- WebLawer/web-law/src/App.jsx
+++ WebLawer/web-law/src/App.jsx
@@ -1,7 +1,9 @@
-
+import FeatureSection from "./components/ui/FeatureSection"
 import { FooterWithSocialLinks } from "./components/ui/FooterWithSocialLinks"
 import { GalleryWithCarousel } from "./components/ui/GalleryWithCarousel"
 import Header from "./components/ui/Header"
+import LogoClouds from "./components/ui/LogoClouds"
+import TeamSection from "./components/ui/TeamSection"
 
 function App() {
 
@@ -9,6 +11,9 @@ function App() {
     <div>
       <Header/>
       <GalleryWithCarousel/>
+      <FeatureSection/>
+      <LogoClouds/>
+      <TeamSection/>
       <FooterWithSocialLinks/>
     </div>
   )
--- WebLawer/web-law/src/components/ui/Header.jsx
+++ WebLawer/web-law/src/components/ui/Header.jsx
@@ -11,6 +11,8 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
+import Logo from '../../assets/logo-dtm-heart.png'
+
 const products = [
   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
   { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
@@ -33,11 +35,11 @@ export default function Headre() {
   return (
     <header className="bg-red-500">
 
-        <nav className="bg-yellow-200 mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="bg-yellow-200 mx-auto flex max-w-7xl items-center justify-between p-5 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
                 <a href="#" className="-m-1.5 p-1.5">
                     <span className="sr-only">Your Company</span>
-                    <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                    <img className="h-14 w-auto" src={Logo} alt="" />
                 </a>
             </div>
             
@@ -50,6 +52,7 @@ export default function Headre() {
                     <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                 </button>
             </div>
+            
             <Popover.Group className="hidden lg:flex lg:gap-x-12">
                 <Popover className="relative">
                     <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
@@ -128,8 +131,8 @@ export default function Headre() {
                 <div className="flex items-center justify-between">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
-                        <img    className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        <img    className="h-14 w-auto"
+                                src={Logo}
                                 alt=""/>
                     </a>
 
@@ -178,7 +181,6 @@ export default function Headre() {
 
                             <a href="#"
                                 className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                
                                 Features
                             </a>
                             <a href="#"
